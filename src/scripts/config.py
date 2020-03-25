0b2f5e4648c65169bc03c4dad4a688749166f892
bitbucketagus <saogmuas@gmail.com>
2020-03-25 21:15:21 +0100

    add results folder.

--- src/scripts/config.py
+++ src/scripts/config.py
@@ -3,6 +3,8 @@ import os
 project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 plots_dir = project_dir + "/results/"
 # plots_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + "/#plots/"
+if not os.path.exists(plots_dir):
+    os.mkdir(plots_dir)
 
 
 def get_filename(filename, experiment, subfolders=[]):
